d6845d8c0c28e06f6afe29ed87d7a19262af4b35
thefarhaan <shaikmohammedfarhaan28@gmail.com>
2026-05-03 18:06:43 +0530

    ww

---
+++ Numpy_Prac/operations.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# a = np.array([[1,2,3],[4,5,6]])
+# print(a)
+# print(a[1,2])
+# print(a[0,1])
+# print(a[0,:])
+# print(a[1,:])
+
+b = np.array([1,2,3])
+c = np.array([4,5,6])
+
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+print(b**2)
+print(b**3)
+
+d =np.array([1,2,3])
+print(d>1)
+print(d==1)
+print(d!=1)
+
+print(np.sqrt([1,4,9]))
+print(np.exp([1,4,9]))
+print(np.sin([0,np.pi/2]))
+
